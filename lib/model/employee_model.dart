52a64dead02806456bf56dd06a167d2ff8309ee3
Akrity Rasaily <akrity.rasaily555@gmail.com>
2026-05-10 19:35:23 +0545

    added employee model

---
+++ lib/model/employee_model.dart
@@ -0,0 +1,19 @@
+// Model - holds the data structure of Employee
+class EmployeeModel {
+  final String empId;
+  final String fullName;
+  final String gender;
+  final String department;
+  final String username;
+  final String password;
+
+  EmployeeModel({
+    required this.empId,
+    required this.fullName,
+    required this.gender,
+    required this.department,
+    required this.username,
+    required this.password,
+  }
+    );
+}
